8c2cf9f1e551b773e609f9469d4324f27ccd0e93
Mauro <asanchez2912@gmail.com>
2021-08-24 13:48:08 -0500

    Add Entity, Model folders

---
+++ api_azure.common/Models/Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace api_azure.common.Models
+{
+    public class Todo
+    {
+        public DateTime CreateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ api_azure.functions/Entities/TodoEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace api_azure.functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreateTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
